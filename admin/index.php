245864b382e0280a1fb12b694c48b841bc7a735a
Kharul Shanto <coderkhayrul@gmail.com>
2021-03-24 17:51:46 +0600

    Category Page Add

--- admin/index.php
+++ admin/index.php
@@ -9,17 +9,9 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Blank Page
-                            <small>Subheading</small>
+                            Welcome To Admin
+                            <small>Author</small>
                         </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
                     </div>
                 </div>
                 <!-- /.row -->
