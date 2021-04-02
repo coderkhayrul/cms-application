ed89ccefc39fbd61295b02f915b2a321fd99498f
Kharul Shanto <coderkhayrul@gmail.com>
2021-04-02 22:19:52 +0600

    Post Message show On Amin Comment Section Dymanic

--- post.php
+++ post.php
@@ -48,18 +48,41 @@
             <?php } ?>
 
             <!-- Blog Comments -->
-
             <!-- Comments Form -->
+            <?php
+            if (isset($_POST['create_comment'])){
+
+                $comment_post_id =$_GET['p_id'];
+                $comment_author = $_POST['comment_author'];
+                $comment_email = $_POST['comment_email'];
+                $comment_content = $_POST['comment_content'];
+
+                $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
+                $query .= "VALUES($comment_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'Unapproved', now())";
+                $create_comment_query = mysqli_query($connection, $query);
+                if (!$create_comment_query){
+                    die("QUERY FAILED".mysqli_error($connection));
+                }
+            }
+            ?>
             <div class="well">
                 <h4>Leave a Comment:</h4>
-                <form role="form">
+                <form action="" method="post" role="form">
                     <div class="form-group">
-                        <textarea class="form-control" rows="3"></textarea>
+                        <label for="author">Author</label>
+                        <input class="form-control" type="text" name="comment_author" placeholder="Enter Name">
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input class="form-control" type="email" name="comment_email" placeholder="Enter Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="comment">Your Comment</label>
+                        <textarea class="form-control" rows="3" name="comment_content" placeholder="Enter Message"></textarea>
+                    </div>
+                    <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
                 </form>
             </div>
-
             <hr>
 
             <!-- Posted Comments -->
