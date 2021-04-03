f649028ce21eb0d1195e6334b8a3049c52be814b
Kharul Shanto <coderkhayrul@gmail.com>
2021-04-04 00:19:26 +0600

    Logout Page Funtion Work

--- admin/index.php
+++ admin/index.php
@@ -1,31 +1,32 @@
 
-<?php include "includes/admin_header.php"; ?>
-    <div id="wrapper">
-        <!-- Navigation -->
-        <?php include "includes/admin_navigation.php"; ?>
-        <div id="page-wrapper">
-            <div class="container-fluid">
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Welcome To Admin
-                            <small>
-                                <?php
-                                if (isset($_SESSION['username'])){
-                                    echo $_SESSION['username'];
-                                }
-                                ?>
-                            </small>
-                        </h1>
-                    </div>
+<?php
+//HEADER
+include "includes/admin_header.php"; ?>
+<div id="wrapper">
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"; ?>
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Welcome To Admin
+                        <small>
+                            <?php
+                            if (isset($_SESSION['username'])){
+                            echo $_SESSION['username'];
+                            }
+                            ?>
+                        </small>
+                    </h1>
                 </div>
-                <!-- /.row -->
             </div>
-            <!-- /.container-fluid -->
+            <!-- /.row -->
         </div>
-        <!-- /#page-wrapper -->
+        <!-- /.container-fluid -->
     </div>
+    <!-- /#page-wrapper -->
+</div>
 <!-- /#wrapper -->
-<!-- Footer-->
-<?php include "includes/admin_footer.php"; ?>
+
---
+++ includes/logout.php
@@ -0,0 +1,1 @@
+<?php session_start();// Finally, destroy the session.session_destroy();header("Location: ../index.php");
