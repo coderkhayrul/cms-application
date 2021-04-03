2da9f00b73820930723ec88ca3d88c736d3885bf
Kharul Shanto <coderkhayrul@gmail.com>
2021-04-03 23:19:10 +0600

    When Login show on session data admin index page

--- admin/index.php
+++ admin/index.php
@@ -10,7 +10,13 @@
                     <div class="col-lg-12">
                         <h1 class="page-header">
                             Welcome To Admin
-                            <small>Author</small>
+                            <small>
+                                <?php
+                                if (isset($_SESSION['username'])){
+                                    echo $_SESSION['username'];
+                                }
+                                ?>
+                            </small>
                         </h1>
                     </div>
                 </div>
