3a3ffe93423e15dba30958fed615c0f8d04e0c62
Kharul Shanto <coderkhayrul@gmail.com>
2021-04-04 10:19:27 +0600

    Add Googel Chart On Admin Dashboard

--- admin/index.php
+++ admin/index.php
@@ -134,6 +134,35 @@ include "includes/admin_header.php"; ?>
                     </div>
                 </div>
             </div>
+            <!-- WIDGET-->
+            <div class="row">
+                <script type="text/javascript">
+                    google.charts.load('current', {'packages':['bar']});
+                    google.charts.setOnLoadCallback(drawChart);
+
+                    function drawChart() {
+                        var data = google.visualization.arrayToDataTable([
+                            ['Year', 'Sales', 'Expenses', 'Profit'],
+                            ['2014', 1000, 400, 200],
+                            ['2015', 1170, 460, 250],
+                            ['2016', 660, 1120, 300],
+                            ['2017', 1030, 540, 350]
+                        ]);
+
+                        var options = {
+                            chart: {
+                                title: 'Company Performance',
+                                subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                            }
+                        };
+
+                        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                        chart.draw(data, google.charts.Bar.convertOptions(options));
+                    }
+                </script>
+                <div id="columnchart_material" style="width: 1080px; height: 600px;"></div>
+            </div>
         </div>
         <!-- /.container-fluid -->
     </div>
